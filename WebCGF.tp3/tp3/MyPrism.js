43383cf7a93f34fe5fde1c11bd10a2d43174e7e0
EZSPECIAL <ftlopes93@gmail.com>
2017-04-18 21:14:44 +0100

    TP3 finished without extra

--- WebCGF.tp3/tp3/MyPrism.js
+++ WebCGF.tp3/tp3/MyPrism.js
@@ -20,35 +20,40 @@ MyPrism.prototype.initBuffers = function() {
 	var normals = [];
 	var indices = [];
 	var increment = 2 * Math.PI / this.slices;
+	var height_inc = 1.0 / this.stacks;
+
 
 	for(var i = 0; i < this.slices; i++) {
-		vertices.push(Math.cos(increment * i), Math.sin(increment * i), 0);
-		vertices.push(Math.cos(increment * i), Math.sin(increment * i), 0);
-		vertices.push(Math.cos(increment * i), Math.sin(increment * i), 1);
-		vertices.push(Math.cos(increment * i), Math.sin(increment * i), 1);
+		for(var j = 0; j <= this.stacks; j++) {
+			vertices.push(Math.cos(increment * i), Math.sin(increment * i), height_inc * j);
+			vertices.push(Math.cos(increment * i), Math.sin(increment * i), height_inc * j);
 
-		normals.push(Math.cos(increment * (i + 0.5)), Math.sin(increment * (i + 0.5)), 0);
-		normals.push(Math.cos(increment * (i - 0.5)), Math.sin(increment * (i - 0.5)), 0);
-		normals.push(Math.cos(increment * (i + 0.5)), Math.sin(increment * (i + 0.5)), 0);
-		normals.push(Math.cos(increment * (i - 0.5)), Math.sin(increment * (i - 0.5)), 0);
+			normals.push(Math.cos(increment * (i + 0.5)), Math.sin(increment * (i + 0.5)), 0);
+			normals.push(Math.cos(increment * (i - 0.5)), Math.sin(increment * (i - 0.5)), 0);
+		}
 	}
 
-	for(var n = 0; n < 2; n++) {
-		for(var i = 0; i < this.slices; i++) {
-			if(i + 1 == this.slices) {
-				indices.push(i * 4 + n);
-				indices.push(2 + n);
-				indices.push(i * 4 + 2 + n);
-				indices.push(0 + n);
-				indices.push(2 + n);
-				indices.push(i * 4 + n);
-			} else {
-				indices.push(i * 4 + n);
-				indices.push(i * 4 + 6 + n);
-				indices.push(i * 4 + 2 + n);
-				indices.push(i * 4 + 4 + n);
-				indices.push(i * 4 + 6 + n);
-				indices.push(i * 4 + n);
+	for(var i = 0; i < this.slices; i++) {
+		var i_inc = (this.stacks + 1) * 2 * i;
+		for(var j = 0; j < this.stacks; j++) {
+			for(n = 0; n < 2; n++) {
+				if(i + 1 == this.slices) {
+					indices.push(i_inc + j * 2 + n);
+					indices.push(j * 2 + 2 + n);
+					indices.push(i_inc + j * 2 + 2 + n);
+					
+					indices.push(j * 2 + n);
+					indices.push(j * 2 + 2 + n);
+					indices.push(i_inc + j * 2 + n);
+				} else {
+					indices.push(i_inc + j * 2 + n);
+					indices.push(i_inc + j * 2 + (this.stacks + 1) * 2 + 2 + n);
+					indices.push(i_inc + j * 2 + 2 + n);
+					
+					indices.push(i_inc + j * 2 + (this.stacks + 1) * 2 + n);
+					indices.push(i_inc + j * 2 + (this.stacks + 1) * 2 + 2 + n);
+					indices.push(i_inc + j * 2 + n);
+				}
 			}
 		}
 	}
@@ -59,5 +64,4 @@ MyPrism.prototype.initBuffers = function() {
 
 	this.primitiveType = this.scene.gl.TRIANGLES;
 	this.initGLBuffers();
-
 };
